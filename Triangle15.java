19de8f8340e4c0060363bb6903d7cd4dfd78598e
martaaa-16 <martaprama@gmail.com>
2024-10-31 12:11:50 +0700

    hi

--- Triangle15.java
+++ Triangle15.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Triangle15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        
         System.out.print("Masukkan nilai N = ");
         int N = sc.nextInt();
         int i = 0;
